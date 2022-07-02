42ccd555300d29303c4f6290eee4e38d51cb9ed2
phr <1466989448@qq.com>
2022-07-02 12:22:29 +0800

    add supervisorscheduler

---
+++ utils/scheduler.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 任务调度
+# 
+# 离线任务调度我们通常可以使用linux crontab或者python APScheduler。
+# 
+# pip install APScheduler==3.5.3
+from apscheduler.executors.pool import ProcessPoolExecutor
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+def test_job():
+    print("test job")
+
+
+def main():
+    # 多进程执行，最大线程数10
+    executors = {
+        'default': ProcessPoolExecutor(3)
+    }
+
+    # 生成一个调度器对象
+    scheduler = BlockingScheduler(executors=executors)
+
+    # 增加一个任务
+    scheduler.add_job(test_job, trigger='interval', seconds=5)
+
+    scheduler.start()
+
+
+if __name__ == '__main__':
+    main()
